6fea2e3cf44001f2ceb57cba9bfea40cd8b981eb
Kartik Reddy <kartikreddy18.kr@gmail.com>
2024-07-22 19:27:12 +0530

    About Page >>> Done!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Home from '@/components/ux/home'
+import About from '@/components/ux/about'
 
 export default function Page() {
   return (
-    <div>
+    <div className="space-y-10">
       <Home />
+      <About />
     </div>
   )
 }
--- src/components/ux/chart.tsx
+++ src/components/ux/chart.tsx
@@ -12,126 +12,16 @@ import {
 } from '@/components/ui/chart'
 
 const chartData = [
-  { date: '2014-01', residential: 5, commercial: 3 },
-  { date: '2014-02', residential: 7, commercial: 2 },
-  { date: '2014-03', residential: 6, commercial: 4 },
-  { date: '2014-04', residential: 8, commercial: 3 },
-  { date: '2014-05', residential: 5, commercial: 5 },
-  { date: '2014-06', residential: 9, commercial: 4 },
-  { date: '2014-07', residential: 10, commercial: 3 },
-  { date: '2014-08', residential: 6, commercial: 6 },
-  { date: '2014-09', residential: 7, commercial: 5 },
-  { date: '2014-10', residential: 5, commercial: 4 },
-  { date: '2014-11', residential: 6, commercial: 3 },
-  { date: '2014-12', residential: 8, commercial: 4 },
-  { date: '2015-01', residential: 9, commercial: 5 },
-  { date: '2015-02', residential: 7, commercial: 6 },
-  { date: '2015-03', residential: 6, commercial: 4 },
-  { date: '2015-04', residential: 8, commercial: 3 },
-  { date: '2015-05', residential: 7, commercial: 5 },
-  { date: '2015-06', residential: 9, commercial: 6 },
-  { date: '2015-07', residential: 10, commercial: 7 },
-  { date: '2015-08', residential: 11, commercial: 6 },
-  { date: '2015-09', residential: 8, commercial: 5 },
-  { date: '2015-10', residential: 7, commercial: 4 },
-  { date: '2015-11', residential: 9, commercial: 5 },
-  { date: '2015-12', residential: 10, commercial: 6 },
-  { date: '2016-01', residential: 11, commercial: 7 },
-  { date: '2016-02', residential: 8, commercial: 6 },
-  { date: '2016-03', residential: 9, commercial: 5 },
-  { date: '2016-04', residential: 10, commercial: 6 },
-  { date: '2016-05', residential: 11, commercial: 7 },
-  { date: '2016-06', residential: 9, commercial: 8 },
-  { date: '2016-07', residential: 10, commercial: 7 },
-  { date: '2016-08', residential: 11, commercial: 8 },
-  { date: '2016-09', residential: 12, commercial: 9 },
-  { date: '2016-10', residential: 11, commercial: 8 },
-  { date: '2016-11', residential: 10, commercial: 7 },
-  { date: '2016-12', residential: 9, commercial: 6 },
-  { date: '2017-01', residential: 10, commercial: 7 },
-  { date: '2017-02', residential: 11, commercial: 8 },
-  { date: '2017-03', residential: 12, commercial: 9 },
-  { date: '2017-04', residential: 13, commercial: 10 },
-  { date: '2017-05', residential: 14, commercial: 9 },
-  { date: '2017-06', residential: 12, commercial: 8 },
-  { date: '2017-07', residential: 13, commercial: 7 },
-  { date: '2017-08', residential: 14, commercial: 8 },
-  { date: '2017-09', residential: 13, commercial: 7 },
-  { date: '2017-10', residential: 12, commercial: 6 },
-  { date: '2017-11', residential: 13, commercial: 7 },
-  { date: '2017-12', residential: 14, commercial: 8 },
-  { date: '2018-01', residential: 15, commercial: 9 },
-  { date: '2018-02', residential: 14, commercial: 8 },
-  { date: '2018-03', residential: 15, commercial: 7 },
-  { date: '2018-04', residential: 16, commercial: 8 },
-  { date: '2018-05', residential: 15, commercial: 9 },
-  { date: '2018-06', residential: 14, commercial: 8 },
-  { date: '2018-07', residential: 15, commercial: 7 },
-  { date: '2018-08', residential: 16, commercial: 8 },
-  { date: '2018-09', residential: 15, commercial: 9 },
-  { date: '2018-10', residential: 14, commercial: 8 },
-  { date: '2018-11', residential: 13, commercial: 7 },
-  { date: '2018-12', residential: 14, commercial: 8 },
-  { date: '2019-01', residential: 15, commercial: 9 },
-  { date: '2019-02', residential: 16, commercial: 8 },
-  { date: '2019-03', residential: 15, commercial: 7 },
-  { date: '2019-04', residential: 14, commercial: 8 },
-  { date: '2019-05', residential: 13, commercial: 9 },
-  { date: '2019-06', residential: 14, commercial: 8 },
-  { date: '2019-07', residential: 15, commercial: 7 },
-  { date: '2019-08', residential: 16, commercial: 8 },
-  { date: '2019-09', residential: 15, commercial: 9 },
-  { date: '2019-10', residential: 14, commercial: 8 },
-  { date: '2019-11', residential: 13, commercial: 7 },
-  { date: '2019-12', residential: 12, commercial: 6 },
-  { date: '2020-01', residential: 13, commercial: 7 },
-  { date: '2020-02', residential: 14, commercial: 8 },
-  { date: '2020-03', residential: 15, commercial: 9 },
-  { date: '2020-04', residential: 16, commercial: 8 },
-  { date: '2020-05', residential: 15, commercial: 7 },
-  { date: '2020-06', residential: 14, commercial: 8 },
-  { date: '2020-07', residential: 15, commercial: 9 },
-  { date: '2020-08', residential: 16, commercial: 8 },
-  { date: '2020-09', residential: 15, commercial: 7 },
-  { date: '2020-10', residential: 14, commercial: 8 },
-  { date: '2020-11', residential: 13, commercial: 7 },
-  { date: '2020-12', residential: 12, commercial: 6 },
-  { date: '2021-01', residential: 13, commercial: 7 },
-  { date: '2021-02', residential: 14, commercial: 8 },
-  { date: '2021-03', residential: 15, commercial: 9 },
-  { date: '2021-04', residential: 16, commercial: 8 },
-  { date: '2021-05', residential: 15, commercial: 7 },
-  { date: '2021-06', residential: 14, commercial: 8 },
-  { date: '2021-07', residential: 15, commercial: 7 },
-  { date: '2021-08', residential: 16, commercial: 8 },
-  { date: '2021-09', residential: 15, commercial: 9 },
-  { date: '2021-10', residential: 14, commercial: 8 },
-  { date: '2021-11', residential: 13, commercial: 7 },
-  { date: '2021-12', residential: 12, commercial: 6 },
-  { date: '2022-01', residential: 13, commercial: 7 },
-  { date: '2022-02', residential: 14, commercial: 8 },
-  { date: '2022-03', residential: 15, commercial: 9 },
-  { date: '2022-04', residential: 16, commercial: 8 },
-  { date: '2022-05', residential: 15, commercial: 7 },
-  { date: '2022-06', residential: 14, commercial: 8 },
-  { date: '2022-07', residential: 15, commercial: 7 },
-  { date: '2022-08', residential: 16, commercial: 8 },
-  { date: '2022-09', residential: 15, commercial: 9 },
-  { date: '2022-10', residential: 14, commercial: 8 },
-  { date: '2022-11', residential: 13, commercial: 7 },
-  { date: '2022-12', residential: 12, commercial: 6 },
-  { date: '2023-01', residential: 13, commercial: 7 },
-  { date: '2023-02', residential: 14, commercial: 8 },
-  { date: '2023-03', residential: 15, commercial: 9 },
-  { date: '2023-04', residential: 16, commercial: 8 },
-  { date: '2023-05', residential: 15, commercial: 7 },
-  { date: '2023-06', residential: 14, commercial: 8 },
-  { date: '2023-07', residential: 15, commercial: 9 },
-  { date: '2023-08', residential: 16, commercial: 8 },
-  { date: '2023-09', residential: 15, commercial: 7 },
-  { date: '2023-10', residential: 14, commercial: 8 },
-  { date: '2023-11', residential: 13, commercial: 7 },
-  { date: '2023-12', residential: 12, commercial: 6 },
+  { year: '2014', residential: 70, commercial: 50 },
+  { year: '2015', residential: 100, commercial: 90 },
+  { year: '2016', residential: 80, commercial: 40 },
+  { year: '2017', residential: 120, commercial: 30 },
+  { year: '2018', residential: 73, commercial: 52 },
+  { year: '2019', residential: 45, commercial: 70 },
+  { year: '2020', residential: 74, commercial: 82 },
+  { year: '2021', residential: 102, commercial: 69 },
+  { year: '2022', residential: 108, commercial: 68 },
+  { year: '2023', residential: 120, commercial: 106 },
 ]
 const chartConfig = {
   projects: {
@@ -153,62 +43,54 @@ export function Component() {
       <CardHeader className='flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row'>
         <div className='grid flex-1 gap-1 text-center sm:text-left'>
           <CardTitle>Area Chart - Interactive</CardTitle>
-          <CardDescription>Showing monthly project completions for over a decade.</CardDescription>
+          <CardDescription>Showing yearly project completions for over a decade.</CardDescription>
         </div>
       </CardHeader>
       <CardContent className='px-2 pt-4 sm:px-6 sm:pt-6'>
         <ChartContainer config={chartConfig} className='aspect-auto h-[250px] w-full'>
-          <AreaChart data={chartData}>
+          <AreaChart accessibilityLayer data={chartData} margin={{left:12, right:12,}}>
             <defs>
-              <linearGradient id='fillDesktop' x1='0' y1='0' x2='0' y2='1'>
+              <linearGradient id='fillResidential' x1='0' y1='0' x2='0' y2='1'>
                 <stop offset='5%' stopColor='var(--color-residential)' stopOpacity={0.8} />
                 <stop offset='95%' stopColor='var(--color-residential)' stopOpacity={0.1} />
               </linearGradient>
-              <linearGradient id='fillMobile' x1='0' y1='0' x2='0' y2='1'>
+              <linearGradient id='fillCommercial' x1='0' y1='0' x2='0' y2='1'>
                 <stop offset='5%' stopColor='var(--color-commercial)' stopOpacity={0.8} />
                 <stop offset='95%' stopColor='var(--color-commercial)' stopOpacity={0.1} />
               </linearGradient>
             </defs>
             <CartesianGrid vertical={false} />
             <XAxis
-              dataKey='date'
+              dataKey='year'
               tickLine={false}
               axisLine={false}
               tickMargin={8}
-              minTickGap={32}
               tickFormatter={(value) => {
                 const date = new Date(value)
                 return date.toLocaleDateString('en-US', {
                   year: 'numeric',
-                  month: 'numeric',
                 })
               }}
             />
             <ChartTooltip
               cursor={false}
               content={
-                <ChartTooltipContent
-                  labelFormatter={(value) => {
-                    return new Date(value).toLocaleDateString('en-US', {
-                      month: 'short',
-                      day: 'numeric',
-                    })
-                  }}
-                  indicator='dot'
-                />
+                <ChartTooltipContent indicator='dot' />
               }
             />
             <Area
               dataKey='commercial'
               type='natural'
-              fill='url(#fillMobile)'
+              fill='var(--color-commercial)'
+              fillOpacity={0.4}
               stroke='var(--color-commercial)'
               stackId='a'
             />
             <Area
               dataKey='residential'
               type='natural'
-              fill='url(#fillDesktop)'
+              fill='var(--color-residential)'
+              fillOpacity={0.4}
               stroke='var(--color-residential)'
               stackId='a'
             />
@@ -219,3 +101,4 @@ export function Component() {
     </Card>
   )
 }
+
